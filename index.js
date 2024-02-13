5259d0fb6d3a9f9014dcedfa94d70ee853bb0983
Eric Winebrenner <ejwinebrenner@gmail.com>
2024-02-13 12:25:26 -0500

    completed notes updated

--- index.js
+++ index.js
@@ -13,7 +13,13 @@
     
     1- Keyword
     2- Name of the function, and parenthesis for parameter
+
+    * DOES get hoisted
 */
+
+function hi() {
+    console.log('Hi!');
+}
 
 //! Expression
 /*
@@ -23,7 +29,13 @@
     }
     
     1- The variable 'hey' is now representative of the function 'hi()'
+
+    * DOES NOT get hoisted
 */
+
+let hey = function hi() {
+    console.log('Hi again!')
+}
 
 //! Invoking
 /*
@@ -37,8 +49,19 @@
     1- This is how we call, or 'invoke' our function
 */
 
+hi();
+hey();
+
 //? Quick Challenge
 /* 
     Create a function that, when invoked, lists out the numbers 1-10
         - hint: How can you cycle through a list of numbers?
 */
+
+function counting() {
+    for(i=1; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+counting();
